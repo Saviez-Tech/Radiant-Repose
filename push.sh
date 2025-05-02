bf92ee0389a6290f9ff9b68e6df8e95396cd8592
treasure56 <simplicitytreasure@gmail.com>
2025-05-02 22:39:00 +0100

    designed: cart and services page

--- push.sh
+++ push.sh
@@ -2,7 +2,7 @@ git add .
 
 ECHO "________________Added________________"
 
-git commit -m "designed: admin staff management page"
+git commit -m "designed: cart and services page"
 
 ECHO "________________Committed________________"
 
