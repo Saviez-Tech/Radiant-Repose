93d492f36d851884804a0d5f66bc2867e24f93a3
Nelson Onuegbu <leonenelson38@gmail.com>
2025-05-14 13:22:56 +0100

    refactor: Simplify user session handling in Axios instance creation

--- src/lib/axios/index.ts
+++ src/lib/axios/index.ts
@@ -1,19 +1,32 @@
 "use server"
-
 import axios from 'axios';
 import { cookies } from 'next/headers';
 
 const createAxiosInstance = async () => {
   const myCookies = await cookies()
-  const cookieValue = await JSON.parse(myCookies.get("user_session")?.value || "")
-
+  
+  const userSession = myCookies.get("user_session")
+  
+  const headers = {
+    'Content-Type': 'application/json',
+    Authorization: ""
+  }
+  
+  if (userSession?.value) {
+    try {
+      const cookieValue = JSON.parse(userSession.value)
+      if (cookieValue.auth_token) {
+        headers.Authorization = `Token ${cookieValue.auth_token}`;
+      }
+    } catch (error) {
+      console.error("Error parsing user_session cookie:", error)
+    }
+  }
+  
   return axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_URL,
     withCredentials: true,
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Token ${cookieValue.auth_token}`,
-    },
+    headers,
   })
 }
 
