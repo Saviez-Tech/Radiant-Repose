fb168cbdf4ef95c8e8eb62cb16b5aaa858dd5db9
treasure56 <simplicitytreasure@gmail.com>
2025-11-03 12:15:05 +0100

    modified services and spa listing error handling

--- push.sh
+++ push.sh
@@ -2,7 +2,7 @@ git add .
 
 ECHO "________________Added________________"
 
-git commit -m "implemented contact api and newsletter"
+git commit -m " modified services and spa listing error handling "
 
 ECHO "________________Committed________________"
 
--- src/components/dashboard/admin-dashboard-components/ProductManagementServerWrapper.tsx
+++ src/components/dashboard/admin-dashboard-components/ProductManagementServerWrapper.tsx
@@ -33,7 +33,7 @@ export async function fetchProductsData(section: "luxury-collection" | "spa-coll
     console.log(err)
     if (axios.isAxiosError(err)) {
       if (err.response) {
-        errorMessage = handleApiError(err.response.data)
+        errorMessage = handleApiError("Something went wrong")
       } else if (err.request) {
         console.log(err.request);
         errorMessage = "Request Failed";
